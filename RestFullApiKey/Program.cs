39c888e32620317d15090f98e48f95bf75b035c3
Giacomo C. <giacomo.corso@gmail.com>
2026-09-19 15:56:05 +0200

    chore: api key setted using Manage User Secret option in project

--- RestFullApiKey/Program.cs
+++ RestFullApiKey/Program.cs
@@ -2,19 +2,14 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add configuration
+var secretKey = builder.Configuration["SecretKey"];
+
 // Add services to the container.
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-/*
- * Configuración OpenApi
- * https://learn.microsoft.com/es-es/aspnet/core/fundamentals/openapi/aspnetcore-openapi?view=aspnetcore-10.0&tabs=visual-studio%2Cvisual-studio-code
- * https://localhost:{PORT}/openapi/v1.json
- * 
- * Configuración Scalar
- * https://scalar.com/products/api-references/integrations/aspnetcore/integration#basic-setup
- */
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -25,7 +20,7 @@ if (app.Environment.IsDevelopment())
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello world!");
+app.MapGet("/", () => $"Hello world! {secretKey}");
 
 app.Run();
 
