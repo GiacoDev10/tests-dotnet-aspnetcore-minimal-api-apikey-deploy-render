d478a61d773b513db63ff6fb9dd04ae43dce12ff
Giacomo C. <giacomo.corso@gmail.com>
2026-09-19 16:56:36 +0200

    feat: Authentication handler

---
+++ RestFullApiKey/Options/ApiKeyAuthenticationOptions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace RestFullApiKey.Options;
+
+public sealed class ApiKeyAuthenticationOptions : AuthenticationSchemeOptions
+{
+    public const string DefaultScheme = "ApiKey";
+    public const string ApiKeyHeaderName = "X-API-KEY";
+}
---
+++ RestFullApiKey/Security/ApiKeyAuthenticationHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using RestFullApiKey.Options;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace RestFullApiKey.Security;
+
+public sealed class ApiKeyAuthenticationHandler(
+    IOptionsMonitor<ApiKeyAuthenticationOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    IConfiguration configuration)
+    : AuthenticationHandler<ApiKeyAuthenticationOptions>(options, logger, encoder)
+{
+
+    private readonly string? _secretKey = configuration["SecretKey"];
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if (string.IsNullOrEmpty(_secretKey))
+        {
+            return Task.FromResult(AuthenticateResult.Fail("API key is not configured on the server."));
+        }
+
+        if (!Request.Headers.TryGetValue(ApiKeyAuthenticationOptions.ApiKeyHeaderName, out var providedKey))
+        {
+            return Task.FromResult(AuthenticateResult.NoResult());
+        }
+
+        // Timing Attack Safe Comparison
+        byte[] providedBytes = Encoding.UTF8.GetBytes(providedKey.ToString());
+        byte[] expectedBytes = Encoding.UTF8.GetBytes(_secretKey);
+
+        if (providedBytes.Length != expectedBytes.Length ||
+            !CryptographicOperations.FixedTimeEquals(providedBytes, expectedBytes))
+        {
+            return Task.FromResult(AuthenticateResult.Fail("Invalid API key."));
+        }
+
+        var claims = new[] {
+            new Claim(ClaimTypes.Name, "static-client") ,
+            new Claim("client_id", "static-client")
+        };
+        var identity = new ClaimsIdentity(claims, Scheme.Name);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
